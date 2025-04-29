e2593543c78dc29cf2edef63366629fa40fc2411
Andrii <pasiura.andriy@lll.kpi.ua>
2025-04-29 17:38:31 +0300

    fix: production build

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: 'https://Andrii12390.github.io/crypto-web/',
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true,
+    }
+})
